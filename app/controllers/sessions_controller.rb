5ba4f055392a9aedd9eed3a68252349d04754f88
Yuta Ushijima <register@yuta-u.com>
2019-01-31 23:10:16 +0900

    ログアウト機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,11 @@ class SessionsController < ApplicationController
 
   end
 
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました。'
+  end
+
   private
 
   def session_params
