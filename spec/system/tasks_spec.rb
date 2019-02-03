98184cbe43246d7134ea71f0dd2d61838e86c868
Yuta Ushijima <register@yuta-u.com>
2019-02-03 16:48:37 +0900

    shared_examplesで処理の共通化

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -15,14 +15,15 @@ RSpec.describe 'タスク管理機能', type: :system do
     click_button 'ログインする'
   end
 
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+    it { expect(page).to have_content '最初のタスク' }
+  end
+
   describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
 
       let(:login_user) { user_a }
-      it 'ユーザーAが作成したタスクが表示されること' do
-        # 作成済みタスクの名称が表示されることを確認
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
 
     context 'ユーザーBがログインしているとき' do
@@ -44,9 +45,7 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit task_path(task_a)
       end
 
-      it 'ユーザーAが作成したタスクの詳細が表示されること' do
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
 
   end
@@ -68,6 +67,5 @@ RSpec.describe 'タスク管理機能', type: :system do
         end
       end
     end
-
   end
 end
