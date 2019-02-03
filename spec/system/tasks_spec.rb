825114e5a8951ab8309a19569e465615d08419b0
Yuta Ushijima <register@yuta-u.com>
2019-02-03 17:25:42 +0900

    新規作成機能のspecを作成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -19,6 +19,34 @@ RSpec.describe 'タスク管理機能', type: :system do
     it { expect(page).to have_content '最初のタスク' }
   end
 
+  describe '新規作成機能' do
+    let(:login_user) { user_a }
+
+    before do
+      visit new_task_url
+      fill_in '名称', with: task_name
+      click_button '登録する'
+    end
+
+    context '新規作成画面で名称を入力したとき' do
+      let(:task_name) { '新規作成のテストを書く' }
+
+      it '正常に登録される' do
+        expect(page).to have_selector '.alert-success', text: '新規作成のテストを書く'
+      end
+    end
+
+    context '新規作成画面で名称を入力しなかったとき' do
+      let(:task_name) { '' }
+
+      it 'バリデーションエラーとなること' do
+        within '#error_explanation' do
+          expect(page).to have_content '名称を入力してください'
+        end
+      end
+    end
+  end
+
   describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
 
@@ -68,4 +96,5 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
+
 end
