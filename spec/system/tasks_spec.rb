524fc6709344ea6e4292ae27c6475f06a9553433
Yuta Ushijima <register@yuta-u.com>
2019-02-03 15:57:23 +0900

    詳細表示機能のspecを作成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'タスク管理機能', type: :system do
-  describe '一覧表示機能' do
-    # ユーザーAを作成しておく
-    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
-    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
-    before do
-      # 作成者がユーザーAであるタスクを作成しておく
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-      # ログインする
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
+  # ユーザーAを作成しておく
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  # 作成者がユーザーAであるタスクを作成しておく
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
 
+  before do
+    # ログインする
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+
+  describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
 
       let(:login_user) { user_a }
@@ -33,5 +34,20 @@ RSpec.describe 'タスク管理機能', type: :system do
        end
 
     end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it 'ユーザーAが作成したタスクの詳細が表示されること' do
+        expect(page).to have_content '最初のタスク'
+      end
+    end
+
   end
 end
