e3ff11852bd54f14b6e1dd445a9f3b0293d4e562
Yuta Ushijima <register@yuta-u.com>
2019-02-08 02:41:29 +0900

    新規登録画面の遷移先を変更/戻るボタンの対応

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,6 +18,12 @@ class TasksController < ApplicationController
     # @task = Task.new(task_params.merge(user_id: current_user.id))
     # current_userに対してリレーションによるメソッドを利用してtaskオブジェクトを作成
     @task = current_user.tasks.new(task_params)
+
+    if params[:back].present?
+      render :new
+      return
+    end
+
     if @task.save
       redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。"
     else
