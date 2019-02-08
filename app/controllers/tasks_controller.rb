dc2541e8ea686ce0651e40db4199cfff6f154ee3
Yuta Ushijima <register@yuta-u.com>
2019-02-08 21:48:40 +0900

    task_paramsにimage属性を追加/詳細画面に画像表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -55,7 +55,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :image)
   end
 
   def set_task
