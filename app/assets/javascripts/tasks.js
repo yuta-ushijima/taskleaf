301980e33203e86a8560d52358195cdf6993e7a8
Yuta Ushijima <register@yuta-u.com>
2019-02-09 15:51:01 +0900

    Ajaxによる削除したタスクの非表示

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,3 +1,16 @@
+// タスク削除時のAjax処理
+document.addEventListener('turbolinks:load', function () {
+  document.querySelectorAll('.delete').forEach(function (a) {
+    a.addEventListener('ajax:success', function () {
+      let td = a.parentNode;
+      let tr = td.parentNode;
+      tr.style.display =  'none';
+    });
+  });
+});
+
+
+// 一覧画面のtd要素の背景色をマウスオーバー時に変更する処理
 document.addEventListener('turbolinks:load', function () {
   document.querySelectorAll('td').forEach(function(td) {
     td.addEventListener('mouseover', function (event) {
@@ -6,7 +19,7 @@ document.addEventListener('turbolinks:load', function () {
 
     td.addEventListener('mouseout', function (event) {
       event.currentTarget.style.backgroundColor = '';
-    })
+    });
   });
-})
+});
 
